335df6c995abb506c770b4a2b7a19264b384f309
adijha <mail@adijha.com>
2020-06-08 20:52:35 +0530

    heroku configure

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
-
 const path = require('path');
-
 const express = require('express');
-const session = require('express-session');
-const colors = require('colors');
-const mongoose = require('mongoose');
 const app = express();
 const crypto = require('crypto');
 const cookie = require('cookie');
@@ -14,7 +9,7 @@ const querystring = require('querystring');
 const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
-const forwardingAddress = 'https://2fca9a52.ngrok.io'; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = 'https://foodics-app.herokuapp.com'; // Replace this with your HTTPS Forwarding address
 
 // db
 const connectDB = require('./connectDB');
@@ -25,133 +20,139 @@ connectDB();
 app.use(express.json());
 
 app.get('/get', (req, res) => {
-	res.send('hi there!');
+  res.send('hi there!');
 });
 
 app.get('/shopify', (req, res) => {
-	scopes = [ 'write_customers', 'read_customers' ];
+  scopes = ['write_customers', 'read_customers'];
 
-	console.log('install route call-->');
-	const shop = req.query.shop;
-	if (shop) {
-		const state = nonce();
-		const redirectUri = forwardingAddress + '/shopify/callback';
-		const installUrl =
-			'https://' +
-			shop +
-			'/admin/oauth/authorize?client_id=' +
-			apiKey +
-			'&scope=' +
-			scopes +
-			'&state=' +
-			state +
-			'&redirect_uri=' +
-			redirectUri;
+  console.log('install route call-->');
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl =
+      'https://' +
+      shop +
+      '/admin/oauth/authorize?client_id=' +
+      apiKey +
+      '&scope=' +
+      scopes +
+      '&state=' +
+      state +
+      '&redirect_uri=' +
+      redirectUri;
 
-		res.cookie('state', state);
+    res.cookie('state', state);
 
-		res.redirect(installUrl);
-	} else {
-		return res
-			.status(400)
-			.send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-	}
+    res.redirect(installUrl);
+  } else {
+    return res
+      .status(400)
+      .send(
+        'Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request'
+      );
+  }
 });
 
 //callback route -->
 app.get('/shopify/callback', (req, res) => {
-	console.log('callback route call -->');
-	let { shop, hmac, code, state } = req.query;
-	Gshop = shop;
-	Ghmac = hmac;
-	const stateCookie = cookie.parse(req.headers.cookie).state;
+  console.log('callback route call -->');
+  let { shop, hmac, code, state } = req.query;
+  Gshop = shop;
+  Ghmac = hmac;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
 
-	if (state !== stateCookie) {
-		return res.status(403).send('Request origin cannot be verified');
-	}
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
 
-	if (shop && hmac && code) {
-		// DONE: Validate request is from Shopify
-		const map = Object.assign({}, req.query);
-		delete map['signature'];
-		delete map['hmac'];
-		const message = querystring.stringify(map);
-		const providedHmac = Buffer.from(hmac, 'utf-8');
-		const generatedHash = Buffer.from(crypto.createHmac('sha256', apiSecret).update(message).digest('hex'), 'utf-8');
-		let hashEquals = false;
+  if (shop && hmac && code) {
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto.createHmac('sha256', apiSecret).update(message).digest('hex'),
+      'utf-8'
+    );
+    let hashEquals = false;
 
-		try {
-			hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac);
-		} catch (e) {
-			hashEquals = false;
-		}
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac);
+    } catch (e) {
+      hashEquals = false;
+    }
 
-		if (!hashEquals) {
-			return res.status(400).send('HMAC validation failed');
-		}
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
 
-		// DONE: Exchange temporary code for a permanent access token
+    // DONE: Exchange temporary code for a permanent access token
 
-		const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-		const accessTokenPayload = {
-			client_id: apiKey,
-			client_secret: apiSecret,
-			code
-		};
-		request
-			.post(accessTokenRequestUrl, { json: accessTokenPayload })
-			.then((accessTokenResponse) => {
-				accessToken = accessTokenResponse.access_token;
-				res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    const accessTokenRequestUrl =
+      'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+    request
+      .post(accessTokenRequestUrl, { json: accessTokenPayload })
+      .then((accessTokenResponse) => {
+        accessToken = accessTokenResponse.access_token;
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 
-				// res.sendFile('index.html', { root: __dirname });
-			})
-			.catch((error) => {
-				// res.sendFile("index.html");
-				res.send(error);
-				console.log('182-->', error);
-			});
-	} else {
-		res.status(400).send('Required parameters missing');
-	}
+        // res.sendFile('index.html', { root: __dirname });
+      })
+      .catch((error) => {
+        // res.sendFile("index.html");
+        res.send(error);
+        console.log('182-->', error);
+      });
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
 });
 
 ////////////////
 app.get('/customers', async (req, res) => {
-	// console.log(tokenRem);
-	console.log(`${accessToken}, tokenRem`.brightMagenta);
+  // console.log(tokenRem);
+  console.log(`${accessToken}, tokenRem`.brightMagenta);
 
-	// console.log(Ghmac);
-	console.log(`${Ghmac}, hmacREM`.brightMagenta);
+  // console.log(Ghmac);
+  console.log(`${Ghmac}, hmacREM`.brightMagenta);
 
-	// console.log(Gshop);
-	console.log(`${Gshop}, shopREM`.brightMagenta);
+  // console.log(Gshop);
+  console.log(`${Gshop}, shopREM`.brightMagenta);
 
-	// console.log(tokenRem, hmacRem, shopRem, 'vars'.cyan);
-	let url = 'https://' + Gshop + '/admin/api/2019-10/customers/count.json';
+  // console.log(tokenRem, hmacRem, shopRem, 'vars'.cyan);
+  let url = 'https://' + Gshop + '/admin/api/2019-10/customers/count.json';
 
-	let options = {
-		method: 'GET',
-		url: url,
-		json: true,
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Shopify-Access-Token': accessToken,
-			'X-Shopify-Hmac-Sha256': Ghmac,
-			'X-Shopify-Shop-Domain': Gshop,
-			'X-Shopify-API-Version': '2019-10'
-		}
-	};
+  let options = {
+    method: 'GET',
+    url: url,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': accessToken,
+      'X-Shopify-Hmac-Sha256': Ghmac,
+      'X-Shopify-Shop-Domain': Gshop,
+      'X-Shopify-API-Version': '2019-10',
+    },
+  };
 
-	request(options)
-		.then((resc) => {
-			console.log(resc);
-			res.send('good');
-		})
-		.catch((err) => {
-			console.error(err);
-			res.send('somehow good');
-		});
+  request(options)
+    .then((resc) => {
+      console.log(resc);
+      res.send('good');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send('somehow good');
+    });
 });
 
 const port = process.env.PORT || 8000;
@@ -159,10 +160,10 @@ const port = process.env.PORT || 8000;
 // if (process.env.NODE_ENV === 'production') {
 app.use(express.static('client/build'));
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 // }
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
